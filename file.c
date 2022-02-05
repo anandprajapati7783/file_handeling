342951e00c23c57d3785b5231b89b63d3946f675
anandprajapati7783 <81299955+anandprajapati7783@users.noreply.github.com>
2022-02-06 01:03:19 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,22 @@
+
+//reading data using  scanf here we can use %s and %c both
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+
+char str[20];
+FILE *fp;
+
+if ((fp=fopen("student.dat","r"))== NULL)
+{
+printf("Error! The file does not exist.");
+exit(0);
+}
+fscanf(fp,"%s", str);
+printf("The character is: %s", str);
+fclose(fp);
+return 0;
+}
